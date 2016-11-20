6c4c2009f3457d6a10c0157a184a67da4a86d5dc
Eon <eoneon123@gmail.com>
2016-11-19 17:37:09 -0800

    added gems and configured: bootstrap, js, rspec, factory_girl and pundit

--- config/application.rb
+++ config/application.rb
@@ -30,6 +30,7 @@ module Notary
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.autoload_paths << File.join(config.root, "lib") 
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
