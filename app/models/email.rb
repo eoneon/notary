8f469a52d94014cc6ab6f7bca23e141538bcc1bb
Eon <eoneon123@gmail.com>
2016-12-07 07:47:20 -0800

    email model: + before_save; + email validations

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,3 +1,11 @@
 class Email < ActiveRecord::Base
   belongs_to :emailable, polymorphic: true
+
+
+  before_save { self.email = email.downcase if email.present? }
+
+  validates :email,
+            presence: true,
+            uniqueness: { case_sensitive: false },
+            length: { minimum: 3, maximum: 254 }
 end
