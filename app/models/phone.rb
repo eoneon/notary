cbbd747c6ddb13c5d7178883eebe1054ad66ac5d
Eon <eoneon123@gmail.com>
2016-12-07 07:51:58 -0800

    phone validations

--- app/models/phone.rb
+++ app/models/phone.rb
@@ -1,4 +1,4 @@
 class Phone < ActiveRecord::Base
   belongs_to :phoneable, polymorphic: true
-  # validates :phone_number, format: { with: /\d{3}-\d{3}-\d{4}/, message: "bad format" }
+  validates :phone_number, format: { with: /\d{3} \d{3} \d{4}/, message: "bad format" }
 end
