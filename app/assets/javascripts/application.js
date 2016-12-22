7586a1c708e879b1bdc99dd67dd3cef44a5483dc
Eon <eoneon123@gmail.com>
2016-12-21 18:59:52 -0800

    trying to toggle js-form mostly unsuccessfully

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,9 @@
 //= require turbolinks
 //= require bootstrap
 //= require_tree .
+
+$(document).ready(function(){
+  $('.js-btn').click(function(event){
+     $(this).parent().find('.js-form:first').toggle();
+   });
+});
