7438b03717af31f3be084776e4b18b0ef6aed220
Eon <eoneon123@gmail.com>
2017-01-18 19:06:40 -0800

    + LineItemsController#update

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -12,6 +12,19 @@ class LineItemsController < ApplicationController
     end
   end
 
+  def update
+    @job = Job.find(params[:job_id])
+    @line_item = @job.assign_attributes(line_item_params)
+
+    if @line_item.save
+      flash[:notice] = "Line item was successfully."
+      redirect_to :back
+    else
+      flash[:notice] = "There was an error saving the line item."
+      render :new
+    end
+  end
+
   private
 
   def line_item_params
